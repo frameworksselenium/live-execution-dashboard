ac53956e222a00ccbd8588cf0a351be028194a68
frameworksselenium <frameworksselenium@gmail.com>
2022-07-18 00:53:09 -0400

    added webpages

--- src/main/java/live/execution/dashboard/UserSpringMicroservicesApplication.java
+++ src/main/java/live/execution/dashboard/UserSpringMicroservicesApplication.java
@@ -2,8 +2,10 @@ package live.execution.dashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
+@Component("live.execution.dashboard")
 public class UserSpringMicroservicesApplication {
 
 	public static void main(String[] args) {
